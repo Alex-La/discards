198f6539cc857485cb95534a8f097c5c01f28b69
Alex-La <lars6162@gmail.com>
2021-03-05 00:10:34 +0300

    Update user model

--- models/User.js
+++ models/User.js
@@ -1,7 +1,15 @@
 const { Schema, model } = require("mongoose");
+const { Phone, PhoneNumberType } = require("mongoose-type-phone");
 
 const schema = new Schema({
   name: { type: String, required: true },
+  surname: { type: String, required: false },
+  phone: {
+    type: Phone,
+    allowBlank: true,
+    defaultRegion: "RU",
+    allowedNumberTypes: PhoneNumberType.MOBILE,
+  },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });

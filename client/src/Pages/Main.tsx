b1843edd6380dc88785113dc2d567bab2b7c37d8
Alex-La <lars6162@gmail.com>
2021-03-05 04:47:30 +0300

    Preset for Main

--- client/src/Pages/Main.tsx
+++ client/src/Pages/Main.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useContext } from "react";
 
 import { BottomNavContext } from "../context/BottomNavContext";
 import SearchInput from "../Components/SearchInput";
+import { Add } from "../Icons";
 
 const Main: React.FC = () => {
   const { setShowNav } = useContext(BottomNavContext);
@@ -15,11 +16,49 @@ const Main: React.FC = () => {
       style={{
         padding: 20,
         marginBottom: 70,
+        position: "absolute",
+        height: "auto",
+        bottom: 0,
+        top: 0,
+        left: 0,
+        right: 0,
         display: "flex",
         flexDirection: "column",
+        justifyContent: "space-between",
       }}
     >
       <SearchInput />
+
+      <div
+        style={{
+          marginTop: 20,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <button
+          className="btn-circle"
+          style={{
+            width: 45,
+            height: 45,
+            backgroundColor: "#FF5151",
+            marginRight: 10,
+          }}
+        >
+          <Add />
+        </button>
+        <p
+          style={{
+            fontWeight: 600,
+            fontSize: 15,
+            lineHeight: "18.29px",
+            color: "#FF5151",
+          }}
+        >
+          Добавить
+        </p>
+      </div>
     </div>
   );
 };

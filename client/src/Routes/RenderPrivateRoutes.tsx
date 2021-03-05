b71c89b2070b1931cc17ca7bc1b8d7e1661ae35e
Alex-La <lars6162@gmail.com>
2021-03-05 03:56:22 +0300

    Add Support page

--- client/src/Routes/RenderPrivateRoutes.tsx
+++ client/src/Routes/RenderPrivateRoutes.tsx
@@ -12,6 +12,8 @@ const Settings = lazy(() => import("../Pages/Settings"));
 const Contacts = lazy(() => import("../Pages/Contacts"));
 const Partners = lazy(() => import("../Pages/Partners"));
 const About = lazy(() => import("../Pages/About"));
+const Support = lazy(() => import("../Pages/Support"));
+const Success = lazy(() => import("../Pages/Success"));
 
 const RenderPrivateRoutes: React.FC = () => {
   const [showNav, setShowNav] = useState<boolean>(true);
@@ -30,6 +32,8 @@ const RenderPrivateRoutes: React.FC = () => {
           <Route path="/contacts" component={Contacts} />
           <Route path="/partners" component={Partners} />
           <Route path="/about" component={About} />
+          <Route path="/support" component={Support} />
+          <Route path="/success" component={Success} />
           <Route path="*" component={NotFound} />
         </Switch>
       </BottomNavContext.Provider>

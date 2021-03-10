a53730328a10c7e13c2fca3eee87f876f254ed27
Alex-La <lars6162@gmail.com>
2021-03-10 03:06:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -39,12 +39,11 @@ app.use(cors());
 if (process.env.NODE_ENV === "production") {
   app.use(
     "/",
-    expressStaticGzip(path.join(__dirname, "build"), {
+    expressStaticGzip(path.join(__dirname, "client", "build"), {
       enableBrotli: true,
       orderPreference: ["br", "gz"],
     })
   );
-  app.use("/", express.static(path.join(__dirname, "client", "build")));
   app.get("*", (_, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
